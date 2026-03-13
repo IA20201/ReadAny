73877a78d71ca33c52ed09f2688303b2175331f4
codedogQBY <1369175442@qq.com>
2026-03-14 02:26:51 +0800

    chore: 删除移动端相关代码和资源文件

--- packages/app-mobile/src-tauri/src/sync/mod.rs
+++
@@ -1,8 +0,0 @@
-pub mod commands;
-pub mod conflict;
-pub mod engine;
-pub mod error;
-pub mod file_sync;
-pub mod models;
-pub mod settings_sync;
-pub mod webdav;
--- packages/app-mobile/src-tauri/src/webview_helper/mod.rs
+++
@@ -1,5 +0,0 @@
-#[cfg(target_os = "ios")]
-mod ios;
-
-#[cfg(target_os = "ios")]
-pub use ios::initialize_keyboard_adjustment;
